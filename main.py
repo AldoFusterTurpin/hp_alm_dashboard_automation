3e9a4380705f88595aade0e4d1afdb2413abc01b
Aldo Fuster <aldoft@hotmail.com>
2019-04-02 13:18:10 +0200

    refactor encoding mess

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
+import parse_xml
 from lxml import etree
 from copy import deepcopy
 import requests
 
 
-def get_defect_printos(session, start_index: int):
+def get_defect_printos(session, start_index: int = None):
     payload = {'query': "{user-90['PrintOS']}"}
 
     if start_index is not None:
         payload['start-index'] = str(start_index)
 
     # important: utf8 encoding
-    print("url params:", end=" ")
+    print("_"*10)
+    print("url params: ")
     [print(k, v) for k, v in payload.items()]
-    print()
+    print("_"*10)
+
     url = "https://alm-1.azc.ext.hp.com/qcbin/rest/domains/IPG_GIB/projects/LFP_Programs/defects"
     response = session.get(url=url, params=payload)
     print("Response status: {}".format(response.status_code))
-    print("*" * 50)
-    xml_response = response.text.encode('utf8')
+    print("*" * 10)
+
+    xml_response = response.text
+    print("type of xml_response: {}".format(type(xml_response)))
     return xml_response
 
 
-def get_defect_printos_and_write_files(session, out_file_name: str, start_index: int):
+def get_defect_printos_and_write_file(session, out_file_name: str, start_index: int):
     xml = get_defect_printos(session, start_index)
-    with open(out_file_name, "wb") as out_file:
+    with open(out_file_name, "w", encoding="utf-8") as out_file:
         out_file.write(xml)
+    return xml
 
 
 def main():
     # The Session object allows you to persist certain parameters across requests.
     # It also persists cookies across all requests made from the Session instance
     with requests.Session() as session:
-        with open("credentials.txt", "r") as credentials_file:
+        with open("credentials.txt", "r", encoding="utf-8") as credentials_file:
             username = credentials_file.readline().strip()
             password = credentials_file.readline().strip()
 
@@ -48,15 +54,16 @@ def main():
 
         # post log-in
         session.post(url="https://alm-1.azc.ext.hp.com/qcbin/rest/site-session")
-
+        # index parameter of the GET request
         start_index = None
-        get_defect_printos_and_write_files(session=session, out_file_name="out0.xml", start_index=start_index)
+        result = get_defect_printos_and_write_file(session=session, out_file_name="out0.xml", start_index=start_index)
         start_index = 101
         while start_index <= 901:
-            get_defect_printos_and_write_files(session=session, out_file_name="out" + str(start_index) + ".xml",
-                                               start_index=start_index)
+            xml = get_defect_printos_and_write_file(session=session, out_file_name="out" + str(start_index) + ".xml", start_index=start_index)
+            result = parse_xml.merge(original_xml=result, second_xml=xml)
             start_index += 100
-
+        with open("result.xml", "w", encoding="utf-8") as result_handler:
+            result_handler.write(result)
 
 
 if __name__ == "__main__":
--- parse_xml.py
+++ parse_xml.py
@@ -6,7 +6,7 @@ def merge(original_xml, second_xml):
     '''Copy all the 'Entity' nodes of 'second_xml' to 'original_xml'(as children of 'Entities' node)'''
 
     # the root of original_xml is the 'Entities node'
-    root_original = etree.fromstring(original_xml.encode('utf-8'))
+    root_accumulated = etree.fromstring(original_xml.encode('utf-8'))
     root_second_xml = etree.fromstring(second_xml.encode('utf-8'))
 
     # print("root: " + root_original.tag)
@@ -15,8 +15,9 @@ def merge(original_xml, second_xml):
     # the nodes I will insert
     entity_nodes = root_second_xml.xpath("Entity")
 
-    [root_original.append(deepcopy(element)) for element in entity_nodes]
+    [root_accumulated.append(deepcopy(element)) for element in entity_nodes]
 
-    ret = etree.tostring(root_original, encoding='utf8', method='xml', pretty_print=False, xml_declaration=False).decode()
+    ret = etree.tostring(root_accumulated, encoding='utf-8', method='xml', pretty_print=False, xml_declaration=False).decode('utf-8')
     ret = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' + ret
     return ret
+
