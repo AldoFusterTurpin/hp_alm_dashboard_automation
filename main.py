bba5a25b9e76049c3e959826fc2b08c5b100213e
Aldo Fuster <aldoft@hotmail.com>
2019-04-03 10:45:43 +0200

    refactor

--- main.py
+++ main.py
@@ -16,21 +16,28 @@ def get_defect_printos(session, start_index: int = None):
     [print(k, v) for k, v in payload.items()]
     print("_"*10)
 
-    url = "https://alm-1.azc.ext.hp.com/qcbin/rest/domains/IPG_GIB/projects/LFP_Programs/defects"
-    response = session.get(url=url, params=payload)
+    response = session.get(url="https://alm-1.azc.ext.hp.com/qcbin/rest/domains/IPG_GIB/projects/LFP_Programs/defects",
+                           params=payload)
     print("Response status: {}".format(response.status_code))
     print("*" * 10)
 
     xml_response = response.text
-    print("type of xml_response: {}".format(type(xml_response)))
+    # print("type of xml_response: {}".format(type(xml_response))) # debug info
     return xml_response
 
 
-def get_defect_printos_and_write_file(session, out_file_name: str, start_index: int):
+def get_defect_printos_and_write_file(session, out_file_name: str, start_index: int = None):
     xml = get_defect_printos(session, start_index)
     with open(out_file_name, "w", encoding="utf-8") as out_file:
         out_file.write(xml)
     return xml
+
+
+def finsih_action(xml_result):
+    '''Given the final xml ('xml_result') after the requests and modifications, this function indicates what to do with
+    the result (write it on disk, send an email or send it to Google drive, etc.)'''
+    with open("result.xml", "w", encoding="utf-8") as out_file:
+        out_file.write(xml_result)
 
 
 def main():
@@ -54,16 +61,17 @@ def main():
 
         # post log-in
         session.post(url="https://alm-1.azc.ext.hp.com/qcbin/rest/site-session")
-        # index parameter of the GET request
-        start_index = None
-        result = get_defect_printos_and_write_file(session=session, out_file_name="out0.xml", start_index=start_index)
+
+        # I accumulate the final xml in 'result'
+        result = get_defect_printos(session=session, start_index=None)
+
+        # index parameter of the GET requests
         start_index = 101
         while start_index <= 901:
-            xml = get_defect_printos_and_write_file(session=session, out_file_name="out" + str(start_index) + ".xml", start_index=start_index)
-            result = parse_xml.merge(original_xml=result, second_xml=xml)
+            tmp_xml = get_defect_printos(session=session, start_index=start_index)
+            result = parse_xml.merge(original_xml=result, second_xml=tmp_xml)
             start_index += 100
-        with open("result.xml", "w", encoding="utf-8") as result_handler:
-            result_handler.write(result)
+        finsih_action(result)
 
 
 if __name__ == "__main__":
