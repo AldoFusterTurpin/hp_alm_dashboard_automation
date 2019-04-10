2303e2f07c935198685c6afc4f39504a2b7742a1
Aldo Fuster <aldoft@hotmail.com>
2019-04-10 12:53:48 +0200

    refactor with functions

--- src/main.py
+++ src/main.py
@@ -1,93 +1,125 @@
-import requests
+# built-in
+import datetime
 import os
+import pathlib
+
+# 3rd party
+import requests
+
+# user defined
 from src import parse_xml
 
 
-def get_defect_printos(session, start_index: int = None) -> str:
-    payload = {'query': "{user-90['PrintOS']}"}
+def check_response_status_code(action_name_performed, response_status_code, expected_status_code):
+    if response_status_code != expected_status_code:
+        raise Exception("Error during" + action_name_performed + ".Response status was: {}".format(response_status_code))
 
-    if start_index is not None:
-        payload['start-index'] = str(start_index)
 
-    url = "https://alm-1.azc.ext.hp.com/qcbin/rest/domains/IPG_GIB/projects/LFP_Programs/defects"
-
+def log_request_info(url, payload, response_status_code):
     print("_" * 30)
     print("NEW REQUEST: \n url: {}".format(url))
     print("----------\nurl params: ")
     [print("{}: {}".format(k, v)) for k, v in payload.items()]
     print("----------")
-
-    response = session.get(url=url, params=payload)
-
-    response_status_code = response.status_code
-    if response_status_code != 200:
-        raise Exception("Error during get_defects_printos. Response status was: {}".format(response_status_code))
-
     print("Response status: {}".format(response_status_code))
     print("_" * 30)
 
-    xml_response = response.text  # type str
-    # print("type of xml_response: {}".format(type(xml_response))) # debug info
-    return xml_response
+
+def get_defect_printos(session, start_index: int = None) -> str:
+    payload = {'query': "{user-90['PrintOS']}"}
+    if start_index is not None:
+        payload['start-index'] = str(start_index)
+
+    url = "https://alm-1.azc.ext.hp.com/qcbin/rest/domains/IPG_GIB/projects/LFP_Programs/defects"
+    response = session.get(url=url, params=payload)
+    response_status_code = response.status_code
+    check_response_status_code(action_name_performed="get_defects_printos",
+                               response_status_code=response_status_code,
+                               expected_status_code=200)
+
+    log_request_info(url=url, payload=payload, response_status_code=response_status_code)
+    xml_response_str = response.text
+    return xml_response_str
 
 
 def get_defect_printos_and_write_file(session, out_file_name: str, start_index: int = None) -> str:
     xml = get_defect_printos(session, start_index)
-    with open(out_file_name, "w", encoding="utf-8") as out_file:
+    with open(out_file_name, "w+", encoding="utf-8") as out_file:
         out_file.write(xml)
     return xml
+
+
+def read_credentials():
+    # credentials_file_path = os.path.join("..", "credentials.txt")
+    credentials_file_path = pathlib.Path("..") / "credentials.txt"
+
+    with open(credentials_file_path, "r", encoding="utf-8") as credentials_file:
+        username = credentials_file.readline().strip()
+        password = credentials_file.readline().strip()
+    return username, password
+
+
+def sign_in(session, username, password):
+    response = session.post(
+        url="https://alm-1.azc.ext.hp.com/qcbin/authentication-point/authenticate?login-form-required=y",
+        data="<alm-authentication><user>" + username + "</user><password>" + password + "</password></alm-authentication>")
+
+    check_response_status_code(action_name_performed="authentication SignIn",
+                               response_status_code=response.status_code,
+                               expected_status_code=200)
+
+
+def open_session(session):
+    '''Action needed after sign-in'''
+    response = session.post(url="https://alm-1.azc.ext.hp.com/qcbin/rest/site-session")
+
+    check_response_status_code(action_name_performed="OpenSession",
+                               response_status_code=response.status_code,
+                               expected_status_code=201)
 
 
 def finish_action(xml_result) -> None:
     '''Given the final xml ('xml_result') after the requests and modifications, this function indicates what to do with
     the result (write it on disk, send an email or send it to Google drive, etc.)'''
-    # print(xml_result)
 
-    file_path = os.path.join("..", "output", "result.xml")
-    with open(file_path, "w", encoding="utf-8") as out_file:
+    now = datetime.datetime.now()
+    file_name = now.strftime("%Y-%m-%d__%H_%M_%S")
+
+    # create (if doesn't exist) folder where we store the output files
+    pathlib.Path("../output").mkdir(parents=True, exist_ok=True)
+    extension = ".xml"
+    output_path = pathlib.Path("../output" + "/" + file_name + extension)
+
+    with open(output_path, "w+", encoding="utf-8") as out_file:
         out_file.write(xml_result)
 
 
 def main():
     # The Session object allows you to persist certain parameters across requests.
     # It also persists cookies across all requests made from the Session instance
-    with requests.Session() as session:
-        file_path = os.path.join("..", "credentials.txt")
-        with open(file_path, "r", encoding="utf-8") as credentials_file:
-            username = credentials_file.readline().strip()
-            password = credentials_file.readline().strip()
+    session = requests.Session()
+    username, password = read_credentials()
 
-        # activating authentication for the session
-        session.auth = (username, password)
+    # activating authentication for the session
+    session.auth = (username, password)
 
-        # post authentication SignIn
-        response = session.post(url="https://alm-1.azc.ext.hp.com/qcbin/authentication-point/authenticate?login-form-required=y",
-                     data="<alm-authentication><user>" + username + "</user><password>" + password + "</password></alm-authentication>")
-        response_status_code = response.status_code
-        if response_status_code != 200:
-            raise Exception("Error during authentication SignIn. Response status was: {}".format(response_status_code))
+    sign_in(session=session, username=username, password=password)
 
-        # check if authenticated (optional)
-        # session.get(url="https://alm-1.azc.ext.hp.com/qcbin/rest/is-authenticated",
-        #       headers={"Content-Type": "application/xml"})
+    open_session(session=session)
 
-        # post OpenSession
-        response = session.post(url="https://alm-1.azc.ext.hp.com/qcbin/rest/site-session")
-        response_status_code = response.status_code
-        if response_status_code != 201:
-            raise Exception("Error during OpenSession. Response status was: {}".format(response_status_code))
+    # final xml accumulated in 'result'
+    result = get_defect_printos(session=session, start_index=None)
 
-        # I will accumulate the final xml in 'result'
-        result = get_defect_printos(session=session, start_index=None)
+    # index parameter of the GET requests
+    start_index = 101
 
-        # index parameter of the GET requests
-        start_index = 101
-        while start_index <= 901:
-            tmp_xml = get_defect_printos(session=session, start_index=start_index)
-            result = parse_xml.merge(original_xml=result, second_xml=tmp_xml)
-            start_index += 100
+    upper_bound = 901
+    while start_index <= upper_bound:
+        tmp_xml = get_defect_printos(session=session, start_index=start_index)
+        result = parse_xml.merge(original_xml=result, second_xml=tmp_xml)
+        start_index += 100
 
-        finish_action(result)
+    finish_action(result)
 
 
 if __name__ == "__main__":
